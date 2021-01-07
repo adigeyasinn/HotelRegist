825d035b9ba655c0ac4bbacf1d696300703c8554
201118063 <201118063@samsun.edu.tr>
2021-01-07 19:49:25 +0300

    Otel tanimlamalari

---
+++ HotelRegist.DataAccess/Abstract/IHotelDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRegist.DataAccess.Abstract
+{
+    public interface IHotelDal
+    {
+
+    }
+}
